4b7d3c6d1798d7581fe3efa489f9ede8c5e56a80
Shadley <shad6ster@gmail.com>
2011-06-02 10:59:14 +0200

    Initial Files

---
+++ local_settings.py
@@ -0,0 +1,23 @@
+DOMAIN = 'rapidcomment.appspot.com'
+
+SITE_NAME = 'Rapid Comment'
+
+COOKIE_DOMAIN = 'rapidcomment.appspot.com'
+
+NS_DOMAIN = 'rapidcomment.appspot.com'
+
+GAE_DOMAIN = 'rapidcomment.appspot.com'
+
+ROOT_NICK = 'shadley@rapidcomment.appspot.com'
+
+TEMPLATE_DEBUG = False
+
+DEFAULT_FROM_EMAIL = 'headhancho@rapidcomment.com'
+
+HOSTED_DOMAIN_ENABLED = False
+
+DEBUG = False
+
+SECRET_KEY = 'bbc3b4bc5d57fef11bc0e1a34785c220'
+
+SSL_LOGIN_ENABLED = False
